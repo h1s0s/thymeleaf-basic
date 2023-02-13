2f69e695444b1a59002a610e84b0159bc0ed8b82
한상선 <hs94122@naver.com>
2023-02-13 21:00:10 +0900

    [한상선] 스프링 MVC 2편 - 타임리프 기본기능 - 리터럴

---
+++ src/main/java/hello/thymeleaf/BasicTwoController.java
@@ -0,0 +1,123 @@
+package hello.thymeleaf;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * packageName  : hello.thymeleaf
+ * fileName     : BasicController
+ * author       : sshan
+ * date         : 2023-02-12
+ * description  :
+ * ========================================================
+ * DATE            AUTHOR              NOTE
+ * --------------------------------------------------------
+ * 2023-02-12          sshan            최초생성
+ */
+@Controller
+@RequestMapping("/basic")
+public class BasicTwoController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+//    /**
+//     * 세션에 담긴 값도 타임리프에서 꺼내어 사용할 수 있다
+//     * @param session
+//     * @return
+//     */
+//    @GetMapping("/basic-objects")
+//    public String basicObjects(HttpSession session) {
+//        session.setAttribute("sessionData", "Hello Session");
+//        return "basic/basic-objects";
+//    }
+//
+//    /**
+//     * 컴포넌트를 사용해 스프링 빈으로 등록한 값도, 타임리프에서 꺼내어 사용할 수 있다
+//     */
+//    @Component("helloBean")
+//    static class HelloBean {
+//        public String hello(String data) {
+//            return "Hello " + data;
+//        }
+//    }
+
+//    /**
+//     * 타임리프는 문자, 숫자, 날짜, URI등을 편리하게 다루는 다양한 유틸리티 객체들을 제공한다.
+//     * @param model
+//     * @return
+//     */
+//    @GetMapping("/date")
+//    public String date(Model model) {
+//        model.addAttribute("localDateTime", LocalDateTime.now());
+//        return "basic/date";
+//    }
+//
+//    /**
+//     * 타임리프에서 URL을 생성할 때
+//     * @param model
+//     * @return
+//     */
+//    @GetMapping("/link")
+//    public String link(Model model) {
+//        model.addAttribute("param1", "data1");
+//        model.addAttribute("param2", "data2");
+//        return "basic/link";
+//    }
+//
+//    @GetMapping("/literal")
+//    public String literal(Model model) {
+//        model.addAttribute("data", "Spring!");
+//        return "basic/literal";
+//    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
+}
